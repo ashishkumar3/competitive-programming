02ee05f18a0ec49de0156c903bc0675bd4da7dc0
ashishkumar3 <ashishk4371@gmail.com>
2021-05-28 16:31:26 +0530

    Circular queue

--- DSA/Queue/CircularQueue.java
+++ DSA/Queue/CircularQueue.java
@@ -12,35 +12,53 @@ public class CircularQueue {
     }
     
     public void enQueue(int data) {
-        // if queue is full
+        // add an element to the queue. Element is added on the rear side.
+        // if queue is full then display queue is full and return
         if(front == 0 && rear == (size - 1) || (rear == front-1)) {
             System.out.println("Queue is full");
             return;
         }
 
-        if(rear == size - 1 && front != 0) {
+        // if queue is empty
+        if(front == -1) {
+            front = 0;
             rear = 0;
-            // insert the element
+            queue.add(rear, data);
+        } else if(rear == size - 1 && front != 0) {
+            rear = 0;
+            queue.add(rear, data);
+        } else {
+            rear = rear + 1;
+            if(front <= rear) {
+                // adding new element
+                queue.add(rear, data);
+            } else {
+                // update the element
+                queue.set(rear, data);
+            }
         }
+
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        int n;
-
-        while(t-- > 0) {
-            n = sc.nextInt();
-
-            System.out.println(solve(n));
+    public int deQueue() {
+        // delete an element from the queue. Element is deleted from the front position.
+        if(front == -1) {
+            System.out.println("Queue is empty!");
+            return -1;
         }
+        
+        int temp = queue.get(front);
 
-        sc.close();
+        if(front == rear) {
+            // only one element
+            front = -1;
+            rear = front;
+        } else if(front == size - 1) {
+            front = 0;
+        } else front = front + 1;
+
+        return temp;
     }
-
-    public static int solve(int n) {
-
-        return 0;
-    }
+            
 }
 
